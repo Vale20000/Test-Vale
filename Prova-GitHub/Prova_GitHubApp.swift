c56a860af1fe33c099ae9449155b8da5f653eb44
Valentino Masetti <valentinomasetti@hotmail.it>
2022-08-18 01:30:12 +0200

    Initial Commit

---
+++ Prova-GitHub/Prova_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prova_GitHubApp.swift
+//  Prova-GitHub
+//
+//  Created by Valentino Masetti on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Prova_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
